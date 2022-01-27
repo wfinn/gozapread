72c35039ca1040a94c902546e6cd70e8a8702425
Finn Westendorf <42862612+wfinn@users.noreply.github.com>
2022-01-27 23:28:23 +0100

    Delete TipUser
    Feature was removed

--- manage.go
+++ manage.go
@@ -5,20 +5,6 @@ import (
 	"net/url"
 )
 
-//TipUser implements Manage/TipUser
-func (c *ZapClient) TipUser(userid, amount uint) error {
-	jsonStr := fmt.Sprintf(`{"id":%d,"amount":%d,"tx":null}`, userid, amount)
-	if resp, err := c.postJSON("Manage/TipUser", jsonStr, true); err == nil {
-		if string(resp) == `{"Result":"Success"}` {
-			return nil
-		} else {
-			return fmt.Errorf("tipping the user wasn't successful")
-		}
-	} else {
-		return err
-	}
-}
-
 //UpdateAboutMe implements Manage/UpdateAboutMe/
 func (c *ZapClient) UpdateAboutMe(aboutme string) error {
 	token, err := c.GetNewToken()
